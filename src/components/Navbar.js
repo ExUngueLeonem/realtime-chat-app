62ba54957bd6dec371d8d469ac2c30d5825e9aeb
Leo <ujfox13@gmail.com>
2021-12-10 19:51:07 +0300

    Navbar routing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,14 +4,22 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Grid from "@mui/material/Grid";
 import { Button } from '@mui/material';
+import { NavLink } from 'react-router-dom';
+import { LOGIN_ROUTE } from '../utils/consts';
 
 const Navbar = () => {
+    const user = true;
     return (
-        <AppBar color="secondary" position="static">
+        <AppBar color="primary" position="static">
             <Toolbar variant="dense">
-                <Grid container justify={"flex"}>
-                    <Button>Логин</Button>
-                    <Button>Выйти</Button>
+                <Grid container justifyContent={"flex-end"}>
+                    {user ?  
+                        <Button color="inherit" variant={"outlined"}>Выйти</Button>
+                        :
+                        <NavLink to={LOGIN_ROUTE}>
+                            <Button color="inherit" variant={"outlined"}>Логин</Button>
+                        </NavLink>
+                    }
                 </Grid>
             </Toolbar>
         </AppBar>
