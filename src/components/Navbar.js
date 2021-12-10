484550e09457a4b3ec1cd23d8cd1f14a817d4a4e
Leo <ujfox13@gmail.com>
2021-12-10 16:27:38 +0300

    navbar in progress

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,17 @@ import React from 'react';
 
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-
+import Grid from "@mui/material/Grid";
+import { Button } from '@mui/material';
 
 const Navbar = () => {
     return (
-        <AppBar position="static">
+        <AppBar color="secondary" position="static">
             <Toolbar variant="dense">
-                
+                <Grid container justify={"flex"}>
+                    <Button>Логин</Button>
+                    <Button>Выйти</Button>
+                </Grid>
             </Toolbar>
         </AppBar>
     );
