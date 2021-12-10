c8eeacfb32b72ce2e0108896f4e73f2c8ac53355
Leo <ujfox13@gmail.com>
2021-12-10 20:49:02 +0300

    login screen

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,26 @@
+import { Button, Container, Grid } from '@mui/material';
+import { Box } from '@mui/system';
 import React from 'react';
 
 const Login = () => {
     return (
-        <div>
-            Login
-        </div>
+        <Container>
+            <Grid container
+                style={{height: window.innerHeight - 50}}
+                alignItems={"center"}
+                justifyContent={"center"}
+                >
+                <Grid style={{width:400, backgroundColor: 'lightgray'}}
+                    container
+                    alignItems={"center"}
+                    direction={"column"}
+                >
+                    <Box p={5}>
+                        <Button variant={"outlined"}>Войти с помощью Google</Button>
+                    </Box>
+                </Grid>
+            </Grid>
+        </Container>
     );
 };
 
